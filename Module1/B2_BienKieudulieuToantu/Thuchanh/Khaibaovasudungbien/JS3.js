b328f9000df93aa1a6a929c9d8a0b4a3189568d8
Tinh04 <mytinh04@gmail.com>
2019-12-27 04:20:19 +0700

    Practice Lession 2

---
+++ Module1/B2_BienKieudulieuToantu/Thuchanh/Khaibaovasudungbien/JS3.js
@@ -0,0 +1,7 @@
+let a = (prompt('Nhap so a'))*1;
+let b = (prompt('Nhap so b'))*1;
+let mod = a % b;
+if (mod === 0)
+    {alert(a + ' la boi cua ' + b);}
+else
+    {alert(a + ' khong la boi cua ' +b);}

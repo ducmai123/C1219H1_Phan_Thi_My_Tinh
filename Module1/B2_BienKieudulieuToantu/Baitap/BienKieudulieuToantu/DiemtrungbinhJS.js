79611c7744cdb3aad9a0b795fbbfe567e627e67f
Tinh04 <mytinh04@gmail.com>
2019-12-27 09:45:24 +0700

    Practice Lession 2

---
+++ Module1/B2_BienKieudulieuToantu/Baitap/BienKieudulieuToantu/DiemtrungbinhJS.js
@@ -0,0 +1,5 @@
+function dtb()
+{
+    document.getElementById("DTB").innerText = "Diem Trung Binh: " + ((document.getElementById("vatli").value)*1
+        + (document.getElementById("hoahoc").value)*1 + (document.getElementById("sinhhoc").value)*1)/3
+}
